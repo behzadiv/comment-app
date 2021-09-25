3f47c75918d290b3d01609a6f69200685a767cf4
Behzad <saeedibehzad@gmail.com>
2021-09-25 13:07:21 +0330

    get data from API by selectedComentId

--- src/components/FullComment.jsx
+++ src/components/FullComment.jsx
@@ -1,12 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const FullComment = () => {
-    return(
-        <div>
-
+const FullComment = ({selectedId}) => {
+    console.log(selectedId);
+    const[selectedComment,setSelectedComponent]=useState(null)
+    useEffect(()=>{
+        if(selectedId)
+        {const selectedComment=async()=>{
+            try{
+                const {data} = await axios.get(`https://jsonplaceholder.typicode.com/comments/${selectedId}`)
+                //console.log(data);
+                setSelectedComponent(data)
+            } 
+            catch(error){
+                console.log(error);
+            }  
+            }
+        selectedComment() }
+    },[selectedId])
+    console.log(selectedComment);
+    if(!selectedComment)return <p>No comment selected !</p>
+    return ( 
+        <div className="fullcomment">
+            <h5>{selectedComment.name}</h5>
+            <p>{selectedComment.email}</p>
+            <p>{selectedComment.body}</p>
         </div>
-    )
+     );
 }
  
 export default FullComment;
