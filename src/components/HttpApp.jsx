3529bad0dd4331fc4f587cfe58493eec926c9a39
Behzad <saeedibehzad@gmail.com>
2021-09-25 10:28:56 +0330

    get data by axios and set myState=> comments

--- src/components/HttpApp.jsx
+++ src/components/HttpApp.jsx
@@ -1,10 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Comment from "./Comment";
 import FullComment from "./FullComment";
 import NewComment from "./NewComment";
+import axios from "axios"
+
 
 const HttpApp = () => {
-   
+  const[comments,setComments]=useState(null)
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response)=>{
+      setComments(response.data.slice(0,4))
+    }).catch((error)=>{
+      console.log(error);
+    })
+    
+  },[])
+  
   return (
     <div>
       <section className="commentSection">
